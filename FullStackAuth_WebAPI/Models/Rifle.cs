503dab9717d9189a087065e3720f8da7b184967e
Colten Bowdoin <coltenbowdoin@gmail.com>
2023-10-31 20:13:48 -0500

    Began coding. Models completed.

---
+++ FullStackAuth_WebAPI/Models/Rifle.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Rifle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Caliber { get; set; }
+        public int ScopeHeight { get; set; }
+        [ForeignKey ("User")]
+        public string UserId { get; set; }
+        [ForeignKey ("ShotData")]
+        public string ShotDataId { get; set; }
+    }
+}
---
+++ FullStackAuth_WebAPI/Models/ShotData.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+namespace FullStackAuth_WebAPI.Models
+{
+    public class ShotData
+    {
+        [Key]
+        public int Id { get; set; }
+        public int TargetRange { get; set; }
+        [ForeignKey ("Rifle")]
+        public string RifleId { get; set; }
+        [ForeignKey ("User")]
+        public string UserId { get; set; }
+    }
+}
