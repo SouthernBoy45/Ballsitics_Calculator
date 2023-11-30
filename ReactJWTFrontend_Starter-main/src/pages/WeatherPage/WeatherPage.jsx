b4ceac4fc2ac392c93a60b32c652c7e40786289d
Colten Bowdoin <coltenbowdoin@gmail.com>
2023-11-29 21:14:24 -0600

    Integrated the weather api. Now figuring out the syntax to render what I need.

--- ReactJWTFrontend_Starter-main/src/pages/WeatherPage/WeatherPage.jsx
+++ ReactJWTFrontend_Starter-main/src/pages/WeatherPage/WeatherPage.jsx
@@ -1,39 +1,42 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const WeatherPage = () => {
- const [weatherDetails, setWeatherDetails] = useState();
- const [zipCode, setZipCode] = useState(0);
+  const [weatherDetails, setWeatherDetails] = useState([]);
+  const [zipCode, setZipCode] = useState(0);
 
- const displayWeatherDetails = async () =>{
+  const displayWeatherDetails = async () => {
+    debugger
     try {
-        let response = await axios.get(
-            `http://api.weatherstack.com/current?access_key=9baaa31981bb905a7485fcae06096008&query=${zipCode}&units=f`);
-            setWeatherDetails(response.data)
-            console.log(response);
-    }catch(error) {
-        console.log(error);
-    }
- }
- function handleSubmit (event) {
+      let response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},US&units=imperial&appid=440ed8745d5dd10a03e50ff8bfac2665`
+      );
+      setWeatherDetails(response.data);
+    } catch (error) {
+      console.log(error);
+    } console.log(weatherDetails);
+  };
+  function handleSubmit(event) {
     event.preventDefault();
     displayWeatherDetails(zipCode);
- }
- return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Enter your Zip Code</label>
-                <input type="number"
-                        name="zipCode"
-                        min="1"
-                        max="99999"
-                        value={zipCode}
-                        onChange={(event) => setZipCode(event.target.value)}/>
-            </form>
-            <h2>Local Weather</h2>
-            {/* <h3>Location: {weatherDetails.location.name}{weatherDetails.location.region}</h3> */}
-        </div>
-    )
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your Zip Code</label>
+        <input
+          type="number"
+          name="zipCode"
+          min="1"
+          max="99999"
+          value={zipCode}
+          onChange={(event) => setZipCode(event.target.value)}
+        />
+      </form>
+      <h2>Local Weather</h2>
+      <h3>Location: {weatherDetails.name} {weatherDetails.main.temp} {weatherDetails.main.pressure}</h3>
+    </div>
+  );
 };
 
 export default WeatherPage;
